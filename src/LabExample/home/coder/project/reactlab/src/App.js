dd8938e2c37e0c3f6d4bb3c94253422fc6d19991
Sheldon Pasciak <sheldon@pasciak.com>
2024-02-10 08:47:00 -0700

    Children as props

---
+++ src/LabExample/home/coder/project/reactlab/src/App.js
@@ -0,0 +1,12 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstName="Sheldon"/>
+            <Heading firstName="Alicia"/>
+        </div>
+    );
+};
+
+export default App;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import Bag from "./Bag";
+import Apples from "./Apples";
+
 function Main() {
-  return <></>;
+  return (
+    <>
+      <Bag children={<Apples color="yellow" number="5" />} />
+      <Bag>
+        <Apples color="green" number="3" />
+      </Bag>
+    </>
+  );
 }
 
 export default Main;
