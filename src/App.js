ff3c2f7f95709ffa9bdde35df12a7d8e05b78bde
Sheldon Pasciak <sheldon@pasciak.com>
2024-02-10 10:50:03 -0700

    use reducer example added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useReducer} from "react";
 
 import Header from "./components/Header";
 import Promo from "./components/Promo";
@@ -11,12 +11,38 @@ import Main from "./components/Main";
 import MealsProvider from "./providers/MealsProvider";
 
 import "./App.css";
+
 import MealsList from "./components/MealsList";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "RIDE":
+      return {
+        money: state.money + 10,
+      };
+    case "FUEL":
+      return {
+        money: state.money -50,
+      };
+    default:
+      return state;
+  }
+}
+
+
 function App(props) {
+
+  const initalState = {money:100};
+  const [state, dispatch] = useReducer(reducer, initalState);
+  
   return (
     <>
     <h1>{props.title}</h1>
+    <h2>{state.money}</h2>
+    <div>
+      <button onClick={() => dispatch({type: "RIDE"})}>Ride</button>
+      <button onClick={() => dispatch({type: "FUEL"})}>Fuel</button>
+    </div>
     <MealsProvider>
       <Header />
       <Main />
